aa0d11a7adf7bfcb0427295973a97d0f56acb1a8
Ovidiu Soare <ovidiuhtm@yahoo.com>
2016-12-26 17:50:45 +0200

    Adding Maven and logger

---
+++ src/main/java/com/github/ovidiusoare/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ovi on 12/26/2016.
+ */
+package com.github.ovidiusoare;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String [] args) {
+        logger.error("Hello world");
+    }
+}
